ee136a543655626166e1f94fe2810dd608d5d81d
LucaBia <gian.luca.99@hotmail.com>
2023-05-28 16:10:45 -0600

    feat: rr-peaks endpoint

--- app3.py
+++ app3.py
@@ -13,6 +13,8 @@ class PolarH10:
     def __init__(self):
         self.bleak_device = None
         self.bleak_client = None
+        self.rr_peaks_lock = asyncio.Lock()
+        self.rr_peaks_data = []
 
     async def connect(self, device):
         self.bleak_device = device
@@ -41,14 +43,29 @@ class PolarH10:
         heart_rate = value[1]
         return heart_rate
 
+    async def add_rr_peak(self, rr_peak):
+        async with self.rr_peaks_lock:
+            self.rr_peaks_data.append(rr_peak)
+
+    async def get_rr_peaks(self):
+        async with self.rr_peaks_lock:
+            peaks = self.rr_peaks_data.copy()
+            self.rr_peaks_data.clear()
+            return peaks
+
 heart_rate_data = []
+rr_peaks_data = []
 polar_device = PolarH10()
 
 async def heart_rate_handler(sender, data):
     heart_rate = data[1]
+    rr_peaks = (data[1] << 8) | data[0]
     heart_rate_data.append(heart_rate)
+    rr_peaks_data.append(rr_peaks)
     timestamp = datetime.now().strftime('%H:%M:%S.%f')
-    print("Heart Rate:", heart_rate, timestamp)
+    print("Heart Rate:", heart_rate, " | ", timestamp)
+    print("RR Peaks:", rr_peaks, " | ", timestamp)
+    await polar_device.add_rr_peak(rr_peaks)
 
 async def scan_polar_devices():
     devices = await BleakScanner.discover()
@@ -90,6 +107,32 @@ async def stream_heart_rate():
             await polar_device.stop_heart_rate_notifications()
 
     return StreamingResponse(generate_heart_rate(), media_type="text/event-stream")
+
+
+@app.get("/rr_peaks")
+async def stream_rr_peaks():
+    async def generate_rr_peaks():
+        try:
+            await polar_device.start_heart_rate_notifications(heart_rate_handler)
+
+            while True:
+                await asyncio.sleep(1)
+                if rr_peaks_data:
+                    rr_peak = rr_peaks_data.pop()
+                    yield f"data: {rr_peak}\n\n"
+
+        except Exception as e:
+            yield f"event: error\ndata: {str(e)}\n\n"
+
+        finally:
+            await polar_device.stop_heart_rate_notifications()
+
+    return StreamingResponse(generate_rr_peaks(), media_type="text/event-stream")
+
+# @app.get("/rr_peaks")
+# async def get_rr_peaks():
+#     peaks = await polar_device.get_rr_peaks()
+#     return {"rr_peaks": peaks}
 
 @app.on_event("shutdown")
 async def shutdown_event():
