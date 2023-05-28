c3807371b383de6c1370dd13723b933464aa3f20
LucaBia <gian.luca.99@hotmail.com>
2023-05-28 14:53:47 -0600

    feat: split connect and heart_rate endpoints

--- app3.py
+++ app3.py
@@ -10,12 +10,18 @@ class PolarH10:
     HEART_RATE_SERVICE_UUID = "0000180d-0000-1000-8000-00805f9b34fb"
     HEART_RATE_CHARACTERISTIC_UUID = "00002a37-0000-1000-8000-00805f9b34fb"
 
-    def __init__(self, bleak_device):
-        self.bleak_device = bleak_device
+    def __init__(self):
+        self.bleak_device = None
+        self.bleak_client = None
 
-    async def connect(self):
+    async def connect(self, device):
+        self.bleak_device = device
         self.bleak_client = BleakClient(self.bleak_device)
         await self.bleak_client.connect()
+
+    async def disconnect(self):
+        if self.bleak_client:
+            await self.bleak_client.disconnect()
 
     async def start_heart_rate_notifications(self, callback):
         await self.bleak_client.start_notify(
@@ -36,6 +42,7 @@ class PolarH10:
         return heart_rate
 
 heart_rate_data = []
+polar_device = PolarH10()
 
 async def heart_rate_handler(sender, data):
     heart_rate = data[1]
@@ -51,16 +58,23 @@ async def scan_polar_devices():
             polar_devices.append(device)
     return polar_devices
 
+@app.get("/connect")
+async def connect_to_polar():
+    try:
+        polar_devices = await scan_polar_devices()
+        if not polar_devices:
+            return {"message": "No Polar devices found"}
+
+        await polar_device.connect(polar_devices[0])
+        return {"message": "Connected to Polar device"}
+
+    except Exception as e:
+        return {"message": str(e)}
+
 @app.get("/heart_rate")
 async def stream_heart_rate():
     async def generate_heart_rate():
         try:
-            polar_devices = await scan_polar_devices()
-            if not polar_devices:
-                yield "event: error\ndata: No Polar devices found\n\n"
-
-            polar_device = PolarH10(polar_devices[0])
-            await polar_device.connect()
             await polar_device.start_heart_rate_notifications(heart_rate_handler)
 
             while True:
@@ -72,7 +86,14 @@ async def stream_heart_rate():
         except Exception as e:
             yield f"event: error\ndata: {str(e)}\n\n"
 
+        finally:
+            await polar_device.stop_heart_rate_notifications()
+
     return StreamingResponse(generate_heart_rate(), media_type="text/event-stream")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await polar_device.disconnect()
 
 if __name__ == "__main__":
     import uvicorn
