ce0c8b401f4d34b2cc260f76bd9123c35850f35e
LucaBia <gian.luca.99@hotmail.com>
2023-06-09 12:18:49 -0600

    feat: static - calculate hrv without pyHRV

--- static.py
+++ static.py
@@ -3,15 +3,13 @@ from fastapi.responses import JSONResponse
 from starlette.responses import PlainTextResponse
 from bleak import BleakScanner, BleakClient
 import asyncio
-import uvicorn
 import struct
-from pyhrv import time_domain
 import math
-
 
 app = FastAPI()
 
 class PolarH10:
+    # Códigos UUID de los servicios y características de Polar H10
     HEART_RATE_SERVICE_UUID = "0000180d-0000-1000-8000-00805f9b34fb"
     HEART_RATE_CHARACTERISTIC_UUID = "00002a37-0000-1000-8000-00805f9b34fb"
 
@@ -59,6 +57,17 @@ async def heart_rate_handler(data):
     heart_rate = struct.unpack('<B', data[1:2])[0]
     heart_rate_data.append(heart_rate)
 
+def calculate_hrv(rr_intervals):
+    if len(rr_intervals) < 2:
+        return None
+
+    differences = [rr_intervals[i] - rr_intervals[i-1] for i in range(1, len(rr_intervals))]
+    squared_differences = [diff ** 2 for diff in differences]
+    mean_squared_diff = sum(squared_differences) / len(squared_differences)
+    rmssd = math.sqrt(mean_squared_diff)
+
+    return rmssd
+
 async def rr_peaks_handler(data):
     flags = struct.unpack('<B', data[0:1])[0]
     rr_interval_present = (flags >> 4) & 0x01
@@ -66,65 +75,14 @@ async def rr_peaks_handler(data):
         rr_interval1 = struct.unpack('<H', data[2:4])[0]
         rr_peaks_data.append(rr_interval1)
         if len(rr_peaks_data) >= 2:
-            await calculate_hrv(rr_peaks_data)
+            hrv_value = calculate_hrv(rr_peaks_data)
+            if hrv_value is not None:
+                hrv_data.append({
+                    "rmssd": hrv_value
+                })
 
 hrv_range_min = 0
 hrv_range_max = 100
-min_rmssd = float('inf')
-max_rmssd = -float('inf')
-
-def normalize_hrv(value, value_min, value_max, range_min, range_max):
-    if value_min == value_max:
-        return range_min  # Evitar división por cero
-    normalized_value = (math.log(value) - math.log(value_min)) / (math.log(value_max) - math.log(value_min)) * (range_max - range_min) + range_min
-    normalized_value = min(max(normalized_value, range_min), range_max)
-    return normalized_value
-
-
-async def calculate_hrv(rr_intervals):
-    # global min_sdnn, max_sdnn, min_rmssd, max_rmssd
-    global min_rmssd, max_rmssd
-
-
-    if len(rr_intervals) < 2:
-        return
-
-    # Calculate HRV measures
-    # sdnn = time_domain.sdnn(rr_intervals)[0]
-    rmssd = time_domain.rmssd(rr_intervals)[0]
-
-    # Handle NaN values
-    # if math.isnan(sdnn) or math.isnan(rmssd):
-    if math.isnan(rmssd):
-        return
-
-    # Update min/max values
-    # min_sdnn = min(min_sdnn, sdnn)
-    # max_sdnn = max(max_sdnn, sdnn)
-    min_rmssd = min(min_rmssd, rmssd)
-    max_rmssd = max(max_rmssd, rmssd)
-
-    # Normalize HRV measures
-    # normalized_sdnn = normalize_hrv(sdnn, min_sdnn, max_sdnn, hrv_range_min, hrv_range_max)
-    normalized_rmssd = normalize_hrv(rmssd, min_rmssd, max_rmssd, hrv_range_min, hrv_range_max)
-
-    # Append HRV data
-    hrv_data.append({
-        # "sdnn": normalized_sdnn,
-        "rmssd": rmssd
-    })
-
-def scale_to_range(value, value_min, value_max, range_min, range_max):
-    scaled_value = ((value - value_min) / (value_max - value_min)) * (range_max - range_min) + range_min
-    return scaled_value
-
-async def scan_polar_devices():
-    devices = await BleakScanner.discover()
-    polar_devices = []
-    for device in devices:
-        if device.name is not None and "Polar" in device.name:
-            polar_devices.append(device)
-    return polar_devices
 
 @app.on_event("shutdown")
 async def shutdown_event():
@@ -140,7 +98,8 @@ async def http_exception_handler(request, exc):
 @app.get("/connect")
 async def connect_to_polar():
     try:
-        polar_devices = await scan_polar_devices()
+        devices = await BleakScanner.discover()
+        polar_devices = [device for device in devices if device.name is not None and "Polar" in device.name]
         if not polar_devices:
             raise HTTPException(status_code=404, detail="No Polar devices found")
 
@@ -204,12 +163,15 @@ async def get_rr_peaks():
 async def get_hrv():
     try:
         if hrv_data:
-            return {"hrv": hrv_data[-1]}
+            hrv_value = round(hrv_data[-1]["rmssd"], 2)
+            hrv_value = max(0, min(hrv_value, 100))
+            return {"hrv": int(hrv_value)}
 
         raise HTTPException(status_code=404, detail="No HRV data available")
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
